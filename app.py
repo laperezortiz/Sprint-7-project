7ee1cbe2534c5fcc0ab9da0e4064d0392d2d1cf7
laperezortiz <206302372+laperezortiz@users.noreply.github.com>
2025-10-26 21:36:48 -0600

    Se ha agregado un archivo llamado app.py con una aplicacion que muestra graficos con botones, otro archivo llamado app_hard.py que muestra graficos por medio de checkboxes, un README.md con descripciones de las aplicaciones, un archivo llamado requirements.txt con las librerias utilizadas, un archico con los datos que se utilizaron para hacer la aplicacion llamado vehicles_us.csv, y un notebook de Jupyter llamado EDA.ipynb, con ejemplos de como crear graficos con plotly.

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+
+car_data = pd.read_csv('vehicles_us.csv')
+
+st.header('Datos de Vehiculos en EUA.')
+
+hist_button = st.button('Construir Histograma')
+scatter_button = st.button('Construir Diagrama de Dispersion')
+
+if hist_button:
+    st.write('Creacion de un histograma para el conjunto de datos de anuncios de venta de coches.')
+    fig = go.Figure(data=[go.Histogram(x=car_data['cylinders'])])
+    fig.update_layout(title_text='Distribución del numero de cilindros')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button:
+    st.write('Creacion de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches.')
+    fig = go.Figure(data=[go.Scatter(x=car_data['cylinders'], y=car_data['price'], mode='markers')])
+    fig.update_layout(title_text='Diagrama de Dispersion del Numero de Cilindros vs Precio', xaxis_title='Numero de Cilindros', yaxis_title='Precio')
+
+    st.plotly_chart(fig, use_container_width=True)
